e92424ee42372e75704e2425a0adbc0a1cd1d7a0
JImumu <jimumu8@163.com>
2018-11-01 10:07:23 +0800

    BUG修复

--- video.js
+++ video.js
@@ -32,7 +32,8 @@
 		    return {
 		        controller: ["$scope", "$element", function ($scope, $element) {
 
-		            $element.bind("touchstart", onTouchStart);
+		            // $element.bind("touchstart", onTouchStart); 存在BUG，改用下面方式
+			    $element.bind("touchmove", onTouchMove);
 		            function onTouchStart(event) {
 		                event.preventDefault();
 		                $element.bind("touchmove", onTouchMove);
@@ -220,179 +221,188 @@
 		<div class="loading" ng-show="isloading">loading...</div>
 	</div>
  */
-angular.module("myApp").controller("video",function($scope,$timeout,$interval,$element,$rootScope){
-	var isplay=false;
-	$scope.sctr=true;
-	$scope.isctr=false;
-	$scope.isloading=false;
-	$scope.showPost=true;
-	$scope.currentTime=0;
-	$scope.duration=0;
-	var hideTime,t1,playTimer,bufferedTimer;
-	function getCurrent(){
+angular.module("myApp").controller("video", function($scope, $timeout, $interval, $element, $rootScope) {
+	var isplay = false;
+	$scope.sctr = true;
+	$scope.isctr = false;
+	$scope.isloading = false;
+	$scope.showPost = true;
+	$scope.currentTime = 0;
+	$scope.duration = 0;
+	var hideTime, t1, playTimer, bufferedTimer;
+
+	function getCurrent() {
 		$scope.currentTime = $($element).find("video")[0].currentTime;
 	}
-	function showCtr(){
-		$scope.sctr=true;
+
+	function showCtr() {
+		$scope.sctr = true;
 	}
-	function hideCtr(){
-		$scope.sctr=false;
+
+	function hideCtr() {
+		$scope.sctr = false;
 	}
-	function showP(){
-		$scope.shouldplay=true;
+
+	function showP() {
+		$scope.shouldplay = true;
 	}
-	function hideP(){
-		$scope.shouldplay=false;
+
+	function hideP() {
+		$scope.shouldplay = false;
 	}
-	function showS(){
-		$scope.shouldpaused=true;
+
+	function showS() {
+		$scope.shouldpaused = true;
 	}
-	function hideS(){
-		$scope.shouldpaused=false;
+
+	function hideS() {
+		$scope.shouldpaused = false;
 	}
-	function play(){
-		$scope.showPost=false;
-		$("video").each(function(index,ele){
+
+	function play() {
+		$scope.showPost = false;
+		$("video").each(function(index, ele) {
 			ele.pause();
 		});
-		$($element).find("video")[0].style.display="block";
+		$($element).find("video")[0].style.display = "block";
 		$($element).find("video")[0].play();
-		setTimeout(function(){
+		setTimeout(function() {
 			$($element).find("video")[0].play();
-		},0)
+		}, 0)
 	}
-	function pause(){
+
+	function pause() {
 		$($element).find("video")[0].pause();
 		$interval.cancel(playTimer);
 	}
 
 	showP();
-	$scope.shc=function(){
-		if($scope.sctr==false){
+	$scope.shc = function() {
+		if ($scope.sctr == false) {
 			showCtr();
-			isplay?showS():showP();
+			isplay ? showS() : showP();
 			$timeout.cancel(t1);
-			t1=$timeout(function(){
+			t1 = $timeout(function() {
 				hideCtr();
 				hideS();
-			},3000)
-		}else{
+			}, 3000)
+		} else {
 			hideCtr();
 			hideS();
 		}
 	}
-	$scope.play = function(){
+	$scope.play = function() {
 		hideP();
 		play();
-		isplay=true;
+		isplay = true;
 	}
-	$scope.pause = function(){
+	$scope.pause = function() {
 		hideS();
 		showP();
 		pause();
-		isplay=false;
+		isplay = false;
 	}
-	$scope.playVideo = function(){
+	$scope.playVideo = function() {
 		var e = window.event;
 		var v = e.target;
-		$($element).find(".controls").css("display","flex");
-		playTimer = $interval(function(){
+		$($element).find(".controls").css("display", "flex");
+		playTimer = $interval(function() {
 			getCurrent();
-			$scope.playedRange = v.currentTime*100/v.duration+"%";
-			if(v.currentTime>=v.duration){
+			$scope.playedRange = v.currentTime * 100 / v.duration + "%";
+			if (v.currentTime >= v.duration) {
 				v.currentTime = 0;
 				$scope.pause();
 			};
-			if(v.currentTime>=v.duration-3){
+			if (v.currentTime >= v.duration - 3) {
 				showCtr();
 			};
-			$scope.left = 100*v.currentTime/v.duration + "%";
-		},100);
+			$scope.left = 100 * v.currentTime / v.duration + "%";
+		}, 100);
 		$scope.getBuffered();
 	}
-	$scope.changeCurrent = function(){
+	$scope.changeCurrent = function() {
 		$timeout.cancel(t1);
-		t1=$timeout(function(){
+		t1 = $timeout(function() {
 			hideCtr();
 			hideS();
-		},3000)
+		}, 3000)
 		var e = window.event;
 		$scope.pause();
 		var x = e.touches[0].clientX;
 		var video = $($element).find("video")[0];
 		var left_padding = $($element).find(".progress").offset().left;
 		var duration_width = $($element).find(".progress").width();
-		var left = (x - left_padding)*100/duration_width;
-		left = left>=100?100:left;
-		left = left<=0?0:left;
+		var left = (x - left_padding) * 100 / duration_width;
+		left = left >= 100 ? 100 : left;
+		left = left <= 0 ? 0 : left;
 		$scope.left = left + "%";
-		video.currentTime = left*video.duration/100;
-		$scope.playedRange = video.currentTime*100/video.duration+"%";
+		video.currentTime = left * video.duration / 100;
+		$scope.playedRange = video.currentTime * 100 / video.duration + "%";
 	}
-	$scope.jumpCurrent = function(){
+	$scope.jumpCurrent = function() {
 		$timeout.cancel(t1);
-		t1=$timeout(function(){
+		t1 = $timeout(function() {
 			hideCtr();
 			hideS();
-		},3000)
+		}, 3000)
 		var e = window.event;
 		var x = e.clientX;
 		var video = $($element).find("video")[0];
 		var left_padding = $($element).find(".progress").offset().left;
 		var duration_width = $($element).find(".progress").width();
-		var left = (x - left_padding)*100/duration_width;
-		left = left>=100?100:left;
-		left = left<=0?0:left;
+		var left = (x - left_padding) * 100 / duration_width;
+		left = left >= 100 ? 100 : left;
+		left = left <= 0 ? 0 : left;
 		$scope.left = left + "%";
-		video.currentTime = left*video.duration/100;
-		$scope.playedRange = video.currentTime*100/video.duration+"%";
+		video.currentTime = left * video.duration / 100;
+		$scope.playedRange = video.currentTime * 100 / video.duration + "%";
 	}
-	$scope.loading = function(){
-		$scope.isloading=true;
+	$scope.loading = function() {
+		$scope.isloading = true;
 	}
-	$scope.noloading = function(){
-		$scope.isloading=false;
+	$scope.noloading = function() {
+		$scope.isloading = false;
 	}
-	$scope.pauseVideo = function(){
+	$scope.pauseVideo = function() {
 		$scope.pause();
 	}
-	$scope.getBuffered = function(){
-		var v=$($element).find("video")[0];
+	$scope.getBuffered = function() {
+		var v = $($element).find("video")[0];
 		$interval.cancel(bufferedTimer);
-		bufferedTimer = $interval(function(){
-			$scope.kkk=[];
+		bufferedTimer = $interval(function() {
+			$scope.kkk = [];
 			var sum = 0;
-			for(var i = 0;i < v.buffered.length;i++){
+			for (var i = 0; i < v.buffered.length; i++) {
 				$scope.kkk[i] = {};
 				$scope.kkk[i].start = v.buffered.start(i);
 				$scope.kkk[i].end = v.buffered.end(i);
-				$scope.kkk[i].width = ($scope.kkk[i].end - $scope.kkk[i].start)*100/v.duration + "%";
-				$scope.kkk[i].left = $scope.kkk[i].start*100/v.duration + "%";
+				$scope.kkk[i].width = ($scope.kkk[i].end - $scope.kkk[i].start) * 100 / v.duration + "%";
+				$scope.kkk[i].left = $scope.kkk[i].start * 100 / v.duration + "%";
 				sum = sum + $scope.kkk[i].end - $scope.kkk[i].start;
 			};
-			if(sum>=v.duration){
+			if (sum >= v.duration) {
 				$interval.cancel(bufferedTimer);
 			};
-		},1000)	
+		}, 1000)
 	}
 	//全屏
-	$scope.fullScreen = function(){
+	$scope.fullScreen = function() {
 		v = $("video")[0];
-		if(v.webkitEnterFullscreen){
+		if (v.webkitEnterFullscreen) {
 			v.webkitEnterFullscreen();
-		}else if(v.mozRequestFullScreen){
+		} else if (v.mozRequestFullScreen) {
 			v.mozRequestFullScreen();
-		}else if(v.requestFullscreen){
+		} else if (v.requestFullscreen) {
 			v.requestFullscreen();
-		}else{
+		} else {
 			alert("您的手机暂不支持全屏播放。")
 		};
 	};
 	//滚出视口暂停
-	$("#content").on("scroll",function(e){
+	$("#content").on("scroll", function(e) {
 		var top = $($element).offset().top;
 		var bottom = $($element).offset().bottom;
-		if(top<-150*document.documentElement.clientWidth/720 || top>$("#content").height()+ 150*document.documentElement.clientWidth/720 ){
+		if (top < -150 * document.documentElement.clientWidth / 720 || top > $("#content").height() + 150 * document.documentElement.clientWidth / 720) {
 			$scope.pause()
 		};
 	});
